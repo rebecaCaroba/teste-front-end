3de59b3c4d2b9b47bb0e72441cb3fb325b7917f3
Rebeca Caroba <rebecacaroba@gmail.com>
2025-10-20 18:01:13 -0300

    ajustando componente Brands

--- src/components/Brands/index.tsx
+++ src/components/Brands/index.tsx
@@ -1,26 +1,73 @@
+'use client'
+import { useState } from 'react';
+import Image from 'next/image'
+import LogoEconverse from '../../../public/LogoEconverse.svg'
 import './style.scss';
+import { IconArrowLeft } from '../Icons/IconArrowLeft';
+import { IconArrowRight } from '../Icons/IconArrowRight';
 
 export function Brands() {
-    const brands = [
-        { id: 1, name: 'Brand 1' },
-        { id: 2, name: 'Brand 2' },
-        { id: 3, name: 'Brand 3' },
-        { id: 4, name: 'Brand 4' },
-        { id: 5, name: 'Brand 5' },
-    ];
+    const [countSlide, setCountSlide] = useState<number>(0);
 
+    function NextSlide() {
+        if (countSlide === 5) {
+            return setCountSlide(0);
+        }
+
+        setCountSlide(countSlide + 1);
+    }
+
+    function PrevSlide() {
+        if (countSlide <= 0) {
+            return setCountSlide(0);
+        }
+
+        setCountSlide(countSlide - 1);
+    }
     return (
         <section className="brands">
-            <div className="brands-container">
-                <h2>Marcas parceiras</h2>
-                <div className="brands-grid">
-                    {brands.map((brand) => (
-                        <div key={brand.id} className="brand-card">
-                            <div className="brand-logo">
-                                <span>{brand.name}</span>
-                            </div>
-                        </div>
-                    ))}
+            <div className="container-center">
+                <h2>Navegue por marcas</h2>
+
+
+                <div className="container-slide-brand">
+                    <div className="slide-cards-buttons-brand">
+                        {
+                            countSlide > 0 &&
+                            <button
+                                className='left'
+                                onClick={() => PrevSlide()}
+                            >
+                                <i><IconArrowLeft /></i>
+                            </button>
+                        }
+
+                        <button
+                            className='right'
+                            onClick={() => NextSlide()}
+                        >
+                            <i><IconArrowRight /></i>
+                        </button>
+                    </div>
+                    <div className="container-slide-cards-brand"
+                        style={{
+                            left: `-${countSlide * 261}px`,
+                            width: `calc(261px * 10)`
+                        }}
+                    >
+                        {
+                            Array.from(Array(10), (item, key) => (
+                                <article key={key}>
+                                    <Image
+                                        src={LogoEconverse}
+                                        width={139}
+                                        height={41}
+                                        alt='Logo Econverse'
+                                    />
+                                </article>
+                            )
+                            )}
+                    </div>
                 </div>
             </div>
         </section>
